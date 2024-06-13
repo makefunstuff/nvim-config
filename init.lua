95717170cd54ce507c7714c58c4828804c4f4b5e
Iurii Plugatariov <iurii.plugatarov@sisugroup.com>
2024-06-13 21:20:00 +0300

    trouble

--- init.lua
+++ init.lua
@@ -37,3 +37,21 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+local actions = require("telescope.actions")
+local open_with_trouble = require("trouble.sources.telescope").open
+
+-- Use this to add more results without clearing the trouble list
+local add_to_trouble = require("trouble.sources.telescope").add
+
+local telescope = require("telescope")
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = { ["<leader>tp"] = open_with_trouble },
+      n = { ["<leader>tn"] = open_with_trouble },
+    },
+  },
+})
+
