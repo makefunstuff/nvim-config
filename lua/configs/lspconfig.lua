4ae658bab109e9fcd2e553ad363e0a7ea08a1f56
makefunstuff <plugatariov@gmail.com>
2024-06-21 17:37:31 +0300

    lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,17 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "zls", "yamlls", "terraformls", "helm_ls", "rust-analyzer", "zls" }
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "zls",
+  "yamlls",
+  "terraformls",
+  "helm_ls",
+  "rust_analyzer",
+  "zls",
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
