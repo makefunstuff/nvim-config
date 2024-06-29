d91fb11fa05e9e9c3f77de19aded5721a195f92c
makefunstuff <plugatariov@gmail.com>
2024-06-29 19:34:39 +0300

    copilot chat

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,19 @@
 return {
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    lazy = true,
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+  {
     "amrbashir/nvim-docs-view",
     lazy = true,
     cmd = "DocsViewToggle",
