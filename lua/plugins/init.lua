66f32ac790ab314ea0181232e830a6071466a35f
makefunstuff <plugatariov@gmail.com>
2024-07-30 12:38:03 +0300

    dap config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -126,6 +126,52 @@ return {
 
   {
     "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+      "julianolf/nvim-dap-lldb",
+    },
+    config = function()
+      local dap = require "dap"
+      local ui = require "dapui"
+
+      require("dapui").setup()
+      require("dap-go").setup()
+
+      require("dap-lldb").setup()
+
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F13>", dap.restart)
+
+      dap.listeners.after.event_initialized["dapui_config"] = ui.open
+      dap.listeners.before.event_terminated["dapui_config"] = ui.close
+      dap.listeners.before.event_exited["dapui_config"] = ui.close
+
+      vim.keymap.set("n", "<leader>du", ui.toggle, { desc = "DAP: UI Toggle" })
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
@@ -141,30 +187,6 @@ return {
         "codelldb",
       },
     },
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "nvim-neotest/nvim-nio" },
-    -- stylua: ignore
-    keys = {
-      { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
-      { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
-    },
-    opts = {},
-    config = function(_, opts)
-      local dap = require "dap"
-      local dapui = require "dapui"
-      dapui.setup(opts)
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open {}
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close {}
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close {}
-      end
-    end,
   },
   {
     "olexsmir/gopher.nvim",
